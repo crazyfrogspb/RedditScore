dc78dd9a9ef17254988452894cc42273e7fcf348
crazyfrogspb <en919@nyu.edu>
2018-03-31 16:58:19 -0400

    Setup modifications

--- setup.py
+++ setup.py
@@ -10,6 +10,8 @@ try:
         readme = f.read()
 except:
     readme = 'Package for performing Reddit-based text analysis'
+	
+INSTALL_REQUIRES = ["setuptools", "spacy>=2.0.10", "tldextract>=2.1.0", "requests>=2.18.4", "nltk>=3.2.5"]
 
 with open('LICENSE') as f:
     license = f.read()
@@ -21,8 +23,9 @@ setup(
     long_description=readme,
     author='Evgenii Nikitin',
     author_email='e.nikitin@nyu.edu',
-    url='https://github.com/kennethreitz/samplemod',
+    url='https://github.com/crazyfrogspb/RedditScore',
     license=license,
     packages=find_packages(exclude=('tests', 'docs')),
 	include_package_data=True,
+	install_requires=INSTALL_REQUIRES,
 )
