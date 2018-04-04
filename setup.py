b7cce3c385e86c28c4c36b6b73da3d541b9fd0cc
crazyfrogspb <en919@nyu.edu>
2018-04-03 20:46:21 -0400

    Documentation change

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open('LICENSE') as f:
 
 setup(
     name='redditscore',
-    version='0.2.0',
+    version='0.3.0',
     description='Package for performing Reddit-based text analysis',
     long_description=readme,
     author='Evgenii Nikitin',
