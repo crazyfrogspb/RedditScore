3f70df13a5880f1133ad8c3bd46f215c54f51c86
crazyfrogspb <en919@nyu.edu>
2018-04-25 11:33:36 -0400

    docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -50,7 +50,8 @@ MOCK_MODULES = ["fastText", "scipy.spatial.distance",
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.napoleon', 'sphinx.ext.viewcode']
+extensions = ['sphinx.ext.autodoc',
+              'sphinx.ext.napoleon', 'sphinx.ext.viewcode']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
