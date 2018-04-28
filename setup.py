81e681f2d57bc0c29e7d349a0072093ac017da44
crazyfrogspb <en919@nyu.edu>
2018-04-28 16:10:49 -0400

    Documentation imports fix

--- setup.py
+++ setup.py
@@ -19,10 +19,11 @@ if not on_rtd:
                         "requests>=2.18.0", "scikit-learn>=0.19.0",
                         "pandas>=0.22.0", "scipy>=1.0.0", "numpy>=1.14.0",
                         "matplotlib>=2.2.0", "beautifulsoup4>=4.6.0",
-                        "adjustText>=0.6.3", "eventlet>=0.22.1"]
+                        "adjustText>=0.6.3", "eventlet>=0.22.1", "dill"]
 else:
     INSTALL_REQUIRES = ["setuptools", "scikit-learn>=0.19.0", "scipy>=1.0.0",
-                        "pandas>=0.22.0", "eventlet>=0.22.1", "requests>=2.18.0"]
+                        "pandas>=0.22.0", "eventlet>=0.22.1", "requests>=2.18.0",
+                        "dill"]
 
 EXTRAS = {
     "nltk": ["nltk>=3.2.5"],
