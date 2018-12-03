8c4b12ce62753d45a13206a833b1867bf5b90c17
crazyfrogspb <en919@nyu.edu>
2018-12-02 23:44:57 -0500

    new version

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ with open('LICENSE') as f:
 
 setup(
     name='redditscore',
-    version='0.7.2',
+    version='0.7.3',
     description='Package for performing Reddit-based text analysis',
     long_description=readme,
     author='Evgenii Nikitin',
